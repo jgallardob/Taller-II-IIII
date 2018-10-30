67485b4fc3b4402c0f5135c2dd992b2b6bf9d118
Claudio Lizama <claudio.lizama2015@alu.uct.cl>
2018-10-30 17:27:20 -0300

    Insercion datos diputados a MongoDB (como objetos)

--- new/Scrape/test.py
+++
@@ -1,6 +0,0 @@
-#Extraccion de datos en forma de objetos para facilitar la insercion a MongoDB
-
-from parlamentarios import Diputado
-
-for diputados in Diputado.getParlamentarios():
-    print diputados #Cambiar por subir a DB
